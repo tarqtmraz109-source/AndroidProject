386e6c17174add881e9d56f5bc280272da82030e
Yousef Al Jadili <yousefaljadili854@gmail.com>
2026-07-29 13:17:54 +0300

    تعديل عنوان القدس وملحمة غزة

--- app/src/main/java/com/example/pricepeek/Shop.java
+++ app/src/main/java/com/example/pricepeek/Shop.java
@@ -38,9 +38,9 @@ public class Shop extends AppCompatActivity {
         shops.add(new ShopModel("سوق السلام", "شارع الجلاء، غزة", "059-345-6789", "⭐ 4.0", "7:00 - 23:00", R.drawable.ic_alsalam_market));
         shops.add(new ShopModel("الشام", "شارع عزالدين، غزة", "059-456-7890", "⭐ 4.8", "24 ساعة", R.drawable.ic_alsham_market));
         shops.add(new ShopModel("سوق غزة للمواد الغذائية", "شارع الوحدة، غزة", "059-567-8901", "⭐ 4.3", "6:00 - 22:00", R.drawable.ic_alsalam_market));
-        shops.add(new ShopModel("ملحمة غزة الطازجة", "شارع التريques، غزة", "059-678-9012", "⭐ 4.6", "8:00 - 20:00", R.drawable.ic_alsalam_market));
+        shops.add(new ShopModel("ملحمة غزة الطازجة", "شارع صلاح الدين، غزة", "059-678-9012", "⭐ 4.6", "8:00 - 20:00", R.drawable.ic_alsalam_market));
         shops.add(new ShopModel("الواحة", "شارع الصناعة، غزة", "059-789-0123", "⭐ 4.1", "9:00 - 19:00", R.drawable.ic_alwaha_furn));
-        shops.add(new ShopModel("القدس", "شارع الم开出، غزة", "059-890-1234", "⭐ 4.4", "10:00 - 20:00", R.drawable.ic_alquds_furn));
+        shops.add(new ShopModel("القدس", "شارع عمر المختار، غزة", "059-890-1234", "⭐ 4.4", "10:00 - 20:00", R.drawable.ic_alquds_furn));
         shops.add(new ShopModel("أثاث روتو", "شارع الرشيد، غزة", "059-901-2345", "⭐ 4.3", "9:00 - 21:00", R.drawable.ic_ruto_furn));
 
         RecyclerView recyclerView = findViewById(R.id.Shops);
